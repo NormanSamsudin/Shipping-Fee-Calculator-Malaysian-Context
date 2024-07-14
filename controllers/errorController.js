4f24cec047c6678aa4a6da2dfe69d4289e9cb4da
Muhammad Norman Bin Samsudin <vs3@boostorder.com>
2024-07-14 20:09:24 +0800

    clean code

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -17,19 +17,6 @@ const handleDuplicateFieldsDB = err => {
   return new AppError(message, 400);
 };
 
-const handleValidationErrorDB = err => {
-  const errors = Object(err.errors).map(el => el.message);
-
-  const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
-};
-
-const handleJWTError = () =>
-  new AppError('Invalid token. Please log in again', 401); //401 for unauthorized access
-
-const handleTokenExpiredError = () =>
-  new AppError('Your token has expired, Please log in again', 401); //401 for unauthorized access
-
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -67,20 +54,13 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production ') {
-    //lepas production ni kne ada space
-    // nak guna destructive sbb nak buat const baru
-    // boleh buang kalau tak nak guna atau tak paham.
-    // cuma nnti error message tu die xleh nak custome bagi senang orang paham jer
+  } else if (process.env.NODE_ENV === 'testing ') {
     let error = JSON.stringify(err);
     error = JSON.parse(error);
     //if ada error yang nak mark as operational
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')
-      error = handleValidationErrorDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTError(); // tak perlu pass error sebab dah custom terus jer
-    if (error.name === 'TokenExpiredError') error = handleTokenExpiredError(); // tak perlu pass error sebab dah custome terus jer
+
     sendErrorProd(error, res);
   }
 };
